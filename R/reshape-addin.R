edf1cf2c73f66e7865e1e15cc55acc554ba16bf8
rmtrane <rmtrane@gmail.com>
2025-04-03 16:56:56 -0500

    Proof-of-concept for using codegrip in .qmd and .rmd files

--- R/reshape-addin.R
+++ R/reshape-addin.R
@@ -45,6 +45,22 @@ addin_reshape <- function() {
 
 addin_reshape_unsafe <- function() {
   context <- rstudioapi::getActiveDocumentContext()
+
+  # Check if file is .qmd or .rmd. First, by file extension of file path.
+  path <- context$path
+  ext <- tolower(tools::file_ext(path))
+
+  if (ext == "qmd" | ext == "rmd") {
+    qmd_or_rmd <- TRUE
+  }
+
+  # If extension is empty, see if contents start with '---' and contains at
+  # least one start of code cell (i.e. "```{r") and one end of code cell
+  # (i.e. "```").
+  if (!qmd_or_rmd) {
+    qmd_or_rmd <- context$contents[1] == "---" & any(grepl(pattern = "^```\\{r", x = context$contents)) & any(context$contents == "```")
+  }
+
   lines <- context$contents
   sel <- context$selection[[1]]$range
 
@@ -56,8 +72,39 @@ addin_reshape_unsafe <- function() {
   line <- sel$start[[1]]
   col <- sel$start[[2]]
 
+  # Adjustments for Quarto and Rmarkdown files
+  if (qmd_or_rmd) {
+    # Get start and end lines for code cells (similar to
+    # quarto::quarto_inspect(...)$fileInformation[[1]]$codeCells, but want
+    # to avoid dependency on quarto R package + want this to work for unsaved
+    # files.)
+    code_cells <- data.frame(
+      start = grep(pattern = "^```\\{r", x = context$contents),
+      end = grep(pattern = "^```$", x = context$contents)
+    )
+
+    # Find cell cursor is in.
+    cur_cell <- code_cells[code_cells$start <= line & line <= code_cells$end, c("start", "end")]
+
+    # Get lines of cell only.
+    lines <- lines[cur_cell$start:cur_cell$end]
+    # "Delete" rows that start with "#|" (i.e. code block options) or "`"
+    # (i.e. start/end of code block)
+    lines[grepl(pattern = "^[#\\||`]", x = lines, fixed = F)] <- ""
+
+    # Adjust line to refer to position in code block
+    line <- line - cur_cell$start + 1
+  }
+
   parse_info <- parse_info(lines = lines)
   out <- reshape_info(line, col, info = parse_info)
+
+  # Adjustments for Quarto and Rmarkdown files part 2
+  if (qmd_or_rmd) {
+    # Adjust start/end line to refer to entire document instead of code block.
+    out$start[["line"]] <- out$start[["line"]] + cur_cell$start - 1
+    out$end[["line"]] <- out$end[["line"]] + cur_cell$start - 1
+  }
 
   pos1 <- rstudioapi::document_position(out$start[["line"]], out$start[["col"]])
   pos2 <- rstudioapi::document_position(out$end[["line"]], out$end[["col"]])
